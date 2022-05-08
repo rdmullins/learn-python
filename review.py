7558411382b324e406e070d4e7807f33ca655c1c
Roger Mullins <38049025+rdmullins@users.noreply.github.com>
2022-05-08 19:55:10 -0400

    Add files via upload
    Quick review as to formatting strings on the fly.

---
+++ review.py
@@ -0,0 +1,7 @@
+name = ""
+age = 0
+
+name = input("What is your name?")
+age = input("And your age?")
+print("OK. So...")
+print (f"Your name is {name}, and you are currently {age} years old. Which means this time next year you should be {age+1}.")
