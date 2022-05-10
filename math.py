b527686681e5a63e42622143a8d6992bda5557ce
Roger Mullins <38049025+rdmullins@users.noreply.github.com>
2022-05-09 20:19:45 -0400

    Add files via upload
    Quick math function review.

---
+++ math.py
@@ -0,0 +1,17 @@
+# Quick review of some Python math elements
+
+x = 0
+y = 0
+
+print("x currently = ", x)
+print("y currently = ", y)
+print(f"x + y = {x+y}")
+print(f"x - y = {x-y}")
+x = 2
+y = 7
+print("Now x is ", x)
+print("And now y is ", y)
+print(f"x + y now is {x+y}")
+print(f"x - y is now {x-y}")
+print(f"x * y is {x*y}")
+print(f"x / y is {x/y}")
