aa6c5df1d90dbd9a6e9f02658cf5bb7ad4c4c0d4
Roger Mullins <38049025+rdmullins@users.noreply.github.com>
2022-05-10 15:13:18 -0400

    Add files via upload
    Basic if/else operation

---
+++ ifelse.py
@@ -0,0 +1,16 @@
+# Basic if loop structure
+
+a = float(input("First: "))
+b = float(input("Second: "))
+op = input("Operation (sum/sub/mul/div): ")
+
+if op == "sum":
+    print(f"a + b = {a+b}")
+elif op == "sub":
+    print(f"a - b =  {a-b}")
+elif op == "mul":
+    print(f"a * b = {a*b}")
+elif op == "div":
+    print(f"a / b = {a/b}")
+else:
+    print("Invalid Operation!")
