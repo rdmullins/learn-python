428dc099189bc58aa9b7c509a695e0730bdfc532
Roger Mullins <38049025+rdmullins@users.noreply.github.com>
2022-05-17 23:46:17 -0400

    Create violet-math.py
    Small math demonstration.

---
+++ violet-math.py
@@ -0,0 +1,6 @@
+# A selection of quick examples of practical math for daughter's homework.
+
+print("The area of a rectangle is length times width."
+input("Length: ", x)
+input("Width: ", y)
+print(f"The area is {x*y} square units.")
